8f54816ecfdba171d67907ab85e0ef69b156e1d9
Tom aan t Goor <tom.aan.t.goor@hva.nl>
2018-06-18 18:32:22 +0200

    Test

--- src/Encryption/Encrypt.php
+++ src/Encryption/Encrypt.php
@@ -7,7 +7,8 @@ namespace Encryption;
 class Encrypt {
     public static function encrypt($value) {
         $hash1 = hash('sha256', "!)@(dkfgbsaewr82398sdjbfsdf'".$value."#$54sdf563254324esd&%^");
-        $hash2 = sha1("[{h34g2534u56489er<:/" . $hash1 . "?;>rguh3r8sdaf8sa83j}]");
+//        $hash2 = sha1("[{h34g2534u56489er<:/".$hash1."?;>rguh3r8sdaf8sa83j}]");
+        $hash2 = sha1("[{h34g2534u56489er<:/".$hash."?;>rguh3r8sdaf8sa83j}]");
         $hash3 = substr($hash2, 0, 30);
         
         return $hash3;
